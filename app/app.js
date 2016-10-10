56e3ba68ee1401505f80ebaa632e89bd9debd9ef
brettc84 <brett.coomer@gmail.com>
2016-10-10 22:59:45 +0200

    First pass at using os-utils

--- app/app.js
+++ app/app.js
@@ -1,4 +1,20 @@
 angular.module('app', []).controller('AppCtrl', function($scope) {
 	var self = this;
 
+	os = require('os-utils');
+	self.systemStats = {};
+
+	
+	os.cpuUsage((cpu) => {
+		self.systemStats.cpuUsage = cpu;
+	});
+
+	self.refreshSystemStats = () => {
+		os.cpuUsage((cpu) => {
+			self.systemStats.cpuUsage = cpu;
+		});
+		self.systemStats.cpus = os.cpuCount();
+		self.systemStats.freemem = os.freemem();
+		self.systemStats.loadAvg = os.loadavg();
+	};
 });
